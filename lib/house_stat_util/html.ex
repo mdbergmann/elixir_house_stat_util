b0469b2e7770e78458586eb1aa4c9c5f33c37f1d
Manfred Bergmann <mb@software-by-mabe.com>
2019-12-29 12:35:32 +0100

    added more html abstraction.

--- lib/house_stat_util/html.ex
+++ lib/house_stat_util/html.ex
@@ -2,18 +2,54 @@ defmodule HouseStatUtil.HTML do
 
   use Eml
   alias Eml.HTML
+  alias Eml.Element, as: E
 
   @type attr :: {:an_attr, binary()} | HTML.attr
+  @type content :: list(E.t()) | E.t() | String.t()
+  @type result :: %E{}
   
-  @spec input([attr()]) :: %Eml.Element{tag: :input, attrs: map()}
+  @spec input([attr()]) :: %E{}
   def input(attrs) do
-    map_attrs = attrs
-    |> Enum.into(%{})
-
-    %Eml.Element{tag: :input, attrs: map_attrs}
+    htag(:input, nil, attrs)
   end
 
-  def generate_string(elem) do
+  @spec html(content(), [attr()]) :: result()
+  def html(content, attrs \\ []) do
+    htag(:html, content, attrs)
+  end  
+  
+  @spec head(content(), [attr()]) :: result()
+  def head(content, attrs \\ []) do
+    htag(:head, content, attrs)
+  end  
+
+  @spec body(content(), [attr()]) :: result()
+  def body(content, attrs \\ []) do
+    htag(:body, content, attrs)
+  end  
+
+  @spec form(content(), [attr()]) :: result()
+  def form(content, attrs \\ []) do
+    htag(:form, content, attrs)
+  end  
+
+  @spec hdiv(content(), [attr()]) :: result()
+  def hdiv(content, attrs \\ []) do
+    htag(:div, content, attrs)
+  end
+
+  @spec htag(
+    atom(),
+    content() | nil,
+    [attr()]) :: result()
+  def htag(tag, content \\ nil, attrs \\ []) do
+    map_attrs = attrs |> Enum.into(%{})
+
+    %E{tag: tag, attrs: map_attrs, content: content}    
+  end
+  
+  @spec render_to_string(E.t()) :: binary()
+  def render_to_string(elem) do
     elem |> Eml.compile(quotes: :double)
   end
   
--- test/html_test.exs
+++ test/html_test.exs
@@ -3,16 +3,96 @@ defmodule HouseStatUtil.HTMLTest do
 
   import HouseStatUtil.HTML
   
-  test "input element" do
+  test "input element - primitive" do
     input_elem = input(id: "some-id", name: "some-name", value: "some-value")
-    |> generate_string
+    |> render_to_string
 
     IO.inspect input_elem
 
     assert String.starts_with?(input_elem, "<input")
-    assert String.contains?(input_elem, "id=\"some-id\"")
-    assert String.contains?(input_elem, "name=\"some-name\"")
-    assert String.contains?(input_elem, "value=\"some-value\"")
+    assert String.contains?(input_elem, ~s(id="some-id"))
+    assert String.contains?(input_elem, ~s(name="some-name"))
+    assert String.contains?(input_elem, ~s(value="some-value"))
+  end
+
+  test "html element - container" do
+    html_elem = html(
+      [
+        htag(:head),
+        htag(:body)
+      ],
+      class: "foo"
+    )
+    |> render_to_string
+
+    IO.inspect html_elem
+
+    assert String.ends_with?(html_elem, 
+      "<html class=\"foo\"><head></head><body></body></html>")
+  end
+  
+  test "head element - container" do
+    head_elem = head(
+      htag(:title, "Foo")
+    )
+    |> render_to_string
+
+    IO.inspect head_elem
+
+    assert head_elem ==  
+      "<head><title>Foo</title></head>"
+  end
+
+  test "body element - container" do
+    body_elem = body(
+      hdiv(nil, id: "foo")
+    )
+    |> render_to_string
+
+    IO.inspect body_elem
+
+    assert body_elem ==  
+      "<body><div id=\"foo\"></div></body>"
+  end
+
+  test "form element - container" do
+    form_elem = form(
+      input(id: "foo")
+    )
+    |> render_to_string
+
+    IO.inspect form_elem
+
+    assert form_elem ==  
+      "<form><input id=\"foo\"/></form>"
+  end
+
+  test "div element - container, empty" do
+    div_elem = hdiv(nil, id: "some-id")
+    |> render_to_string
+
+    IO.inspect div_elem
+
+    assert div_elem == ~s(<div id="some-id"></div>)
+  end
+
+  test "div element - container, none empty" do
+    div_elem = hdiv([input(id: "input-id")], id: "some-id")
+    |> render_to_string
+
+    IO.inspect div_elem
+
+    assert div_elem ==
+      ~s(<div id="some-id"><input id="input-id"/></div>)
+  end
+
+  test "custom htag" do
+    elem = htag(:h2, "content", class: "foo")
+    |> render_to_string()
+
+    IO.inspect elem
+
+    assert elem == ~s(<h2 class="foo">content</)
   end
   
 end
