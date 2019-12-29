0839976c5c360945587758bcf45cba14fefe8e51
Manfred Bergmann <mb@software-by-mabe.com>
2019-12-29 17:05:24 +0100

    added router test for submit readers.

--- test/router_test.exs
+++ test/router_test.exs
@@ -29,4 +29,15 @@ defmodule HouseStatUtil.RouterTest do
     assert conn.status == 200
     assert String.contains?(conn.resp_body, "Submit values to openHAB")
   end
+
+  test "post on /submit_readers" do
+    conn = :post
+    |> conn("/submit_readers")
+    |> Router.call(@opts)
+
+    IO.inspect conn
+
+    assert conn.state == :sent
+    assert conn.status == 200
+  end
 end
