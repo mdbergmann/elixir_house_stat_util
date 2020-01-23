044bf243edf02c835c9b9192e4f7d1766147c4c5
Manfred Bergmann <mb@software-by-mabe.com>
2020-01-23 20:53:58 +0100

    version bump to 0.2.0.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HouseStatUtil.MixProject do
   def project do
     [
       app: :elixir_house_stat_util,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.9",
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
